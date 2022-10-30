1f7ee39c40d13bbbf7caf7d0dde810a19d2050b6
AC59852 <austin.caron1@gmail.com>
2022-10-29 22:57:55 -0400

    added basic html and simple vue supported functionality for all components of SPA. added assets, main.css and reset.css

---
+++ src/components/NavComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <header id="header">
+    <h2 class="header__nav">Dodds</h2>
+    <div class="hero__navBtn" aria-label="Navigation Button">
+      <span class="line"></span>
+      <span class="line"></span>
+      <span class="line"></span>
+    </div>
+    <nav>
+      <ul class="header__list">
+        <li class="header__item" v-for="(link, index) in links" :key="index">
+          <a :href="'#' + link.location" class="header__link">{{link.location}}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        { location: 'home' },
+        { location: 'about' },
+        { location: 'products' },
+        { location: 'faq' },
+        { location: 'contact' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/ProductsComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <section id="products">
+    <h2 class="products__title title--large title--underscore">Featured Items</h2>
+    <ul class="products__wrapper">
+      <li class="products__item" v-for="(product, index) in products" :key="index">
+        <span class="project__expand">+</span>
+        <img :src="product.img" class="products__img" :alt="product.alt">
+        <h3 class="products__name">{{ product.name }}</h3>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        {
+          name: 'Swords',
+          img: '/swords.jpg',
+          alt: 'Image of a sword created by Dodds Family Smithing',
+        },
+        {
+          name: 'Axes',
+          img: '/axes.jpg',
+          alt: 'Image of a steel axe created by Dodds Family Smithing',
+        },
+        {
+          name: 'Armour',
+          img: '/armour.jpg',
+          alt: 'Image of an iron helmet, fur coat, and leather wraps created by Dodds Family Smithing',
+        },
+        {
+          name: 'Knives (Folding)',
+          img: '/knives_folding.jpg',
+          alt: 'Image of a lead folding knife with wooden base created by Dodds Family Smithing',
+        },
+        {
+          name: 'Knives (Straight)',
+          img: '/knives_straight.jpg',
+          alt: 'Image of a steel straight knife with wooden base created by Dodds Family Smithing',
+        },
+        {
+          name: 'LARPing',
+          img: '/larping.jpg',
+          alt: 'Image of a man wearing knights armour and a straight sword created by Dodds Family Smithing',
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
